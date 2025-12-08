4d0a4577db249e124b4ded2f03afe130adb113bf
sajina0 <pradhananga.sajina11@gmail.com>
2025-12-08 14:43:25 +0545

    updated project files

--- lab9/Lab10/connect.php
+++
@@ -1,9 +0,0 @@
-    <?php
-    $host="localhost";
-    $username="root";
-    $password="";
-    $db="dav";
-    $conn = mysqli_connect($host,$username,$password,$db);
-    if(!$conn){echo die("<h1>database is not connected: ".mysqli_connect_error()."</h1><hr>");
-    }
-     ?>
